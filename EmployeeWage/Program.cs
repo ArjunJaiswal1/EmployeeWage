b9cc6736d05d542b0786d436f11641849d0d4ae3
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-03-22 00:08:24 +0530

    [Arjun] Add ; Added check employee is present or not

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Employeewage
+{
+    public static void Main()
+    {
+        int Fulltime = 1;
+        Random random_num = new Random();
+        int checkemp = random_num.Next(0, 2);
+        if (checkemp == Fulltime)
+        {
+            Console.WriteLine("Employee Is Present");
+        }
+        else
+        {
+            Console.Write("Employee is Absent");
+        }
+
+    }
+}
